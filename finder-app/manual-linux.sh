d822450325fec4f6f3dcd12141feab9f1192e44b
Biplav Poudel <biplavpoudel764@gmail.com>
2026-01-03 23:53:00 +0545

    added script to make device nodes and gzipped initramfs.cpio

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,7 +35,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Kernel Building Steps
+    #Kernel Building Steps
 	# QEMU build - clean: deep cleans kernel build tree by removing .config files
 	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}mrproper
 	
@@ -62,7 +62,7 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+#Create necessary base directories
 ROOTFS=${OUTDIR}/rootfs
 
 mkdir -p ${ROOTFS}
@@ -77,7 +77,7 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    #Configure busybox
 	make distclean
 	make defconfig
 
@@ -85,7 +85,7 @@ else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+#Make and install busybox
 make ARCH=${ARCH} CROSS_COMPILER=${CROSS_COMPILE}
 make CONFIG_PREFIX=${ROOTFS} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
@@ -93,7 +93,7 @@ echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+#Add library dependencies to rootfs
 
 cd "$ROOTFS"
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
@@ -103,13 +103,23 @@ cp ${SYSROOT}/lib64/lib64/libm.so.6 ${ROOTFS}/lib64/
 cp ${SYSROOT}/lib64/libresolv.so.2 ${ROOTFS}/lib64/
 cp ${SYSROOT}/lib64/libc.so.6 ${ROOTFS}/lib64/
 
-# TODO: Make device nodes
+#Make device nodes
+sudo mknod -m 666 ${ROOTFS}/dev/null c 1 3
+sudo mknod -m 666 ${ROOTFS}/dev/console c 5 1
+sudo mknod -m 666 ${ROOTFS}/dev/zero c 1 5   # not specifically mentioned in assignment but I thought it is needed for infinite stream of 0s
 
-# TODO: Clean and build the writer utility
+#Clean and build the writer utility
+cd "${FINDER_APP_DIR}"
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}   #gcc is appended in Makefile
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+# Copy the finder related scripts and executables to the /home directory on the target rootfs
+cp -r ${FINDER_APP_DIR} ${ROOTFS}/home/
 
-# TODO: Chown the root directory
+#Chown the root directory
+cd "$ROOTFS"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 
-# TODO: Create initramfs.cpio.gz
+#Create initramfs.cpio.gz
+gzip -f ${OUTDIR}/initramfs.cpio
+
