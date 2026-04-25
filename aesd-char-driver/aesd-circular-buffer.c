7ff395856b7f360248f9ac554a7766d964fdd9a7
Biplav Poudel <biplavpoudel764@gmail.com>
2026-04-25 22:46:15 +0545

    circular buffer completed

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -10,6 +10,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #ifdef __KERNEL__
 #include <linux/string.h>
 #else
@@ -31,31 +32,49 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    uint8_t next_entry = 0;
-    size_t remaining_offset = char_offset;
+    uint8_t cur_entry = buffer->out_offs;   // current entry starts from out_offset!
+    size_t remaining_char_offset = char_offset;
 
-    // Returnss void if the pointers point to nowhere
+    // Returns void if the pointers point to nowhere
     if ((buffer == NULL) || (entry_offset_byte_rtn == NULL)) return NULL;
 
     // Returns NULL if buffer pointer is also empty
-    if (!buffer->full && (buffer->out_offs = buffer->in_offs)) return NULL;
+    if (!buffer->full && (buffer->out_offs == buffer->in_offs)) return NULL;
+    // printf("\n\nENTERING  CODE LOGIC>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+    // printf("The given char_offset is: %ld\n", char_offset);
 
-    while(true)
+    // it is not guarenteed that the read/write buffer starts together from 0th position, so we find valid entries thorugh relative positions.
+    // i.e. inoffs (writeops) = 2, outoffs (readops) = 3, MAX = 4. then valid entries starts from 3 (last)->0(first)->1(second), so only -1 mod 4 = 3.
+    size_t total_valid_entries = buffer->full ? AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED : (buffer->in_offs - buffer->out_offs) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    size_t read_entries = 0;
+
+    // we place the condition so the loop only goes over the valid entries starting from "out_offs" and not to infinity!
+    while (read_entries < total_valid_entries && buffer->entry[cur_entry].buffptr != NULL)
     {
-        // returns NULL if not enough entry for char_offset
-        if (buffer->entry[next_entry].buffptr == NULL) return NULL;
-
-        else if (buffer->entry[next_entry].size < remaining_offset)
+        // if remaining_char_offset is larger than or equal to the new entry, we subtract the remaining_char_offset and goto new entry
+        if (buffer->entry[cur_entry].size <= remaining_char_offset)
         {
-            remaining_offset -= buffer->entry[next_entry].size;
-            next_entry = (next_entry + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+            // printf("The size of the current entry %s is: %ld\n",buffer->entry[cur_entry].buffptr, buffer->entry[cur_entry].size);
+            remaining_char_offset -= buffer->entry[cur_entry].size;
+            // printf("The updated remaining char_offset is %ld\n", remaining_char_offset);
+
+            cur_entry = (cur_entry + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+            // printf("The new value of new current entry is %s\n", buffer->entry[cur_entry].buffptr);
+            read_entries ++;
+            // printf("The no. of read_entries out of %ld are: %ld\n\n", total_valid_entries, read_entries);
             continue;
         }
-        *(entry_offset_byte_rtn) = remaining_offset;
-        break;   
+
+        // if the remaining_offset is smaller than the current entry, it means the char_offset points to the "current entry" at remaining_char_offset position!
+        // printf("------------\n");
+        // printf("The total read_entries out of %ld are: %ld\n", total_valid_entries, ++read_entries);
+        // printf("The final remaining char_offset is %ld\n", remaining_char_offset);
+        // printf("The final entry is %s\n\n`", buffer->entry[cur_entry].buffptr);
+        *entry_offset_byte_rtn = remaining_char_offset;
+        return &buffer->entry[cur_entry];
     }
-    return &buffer->entry[next_entry];
-}
+    return NULL;
+    }
 
 /**
 * Adds entry @param add_entry to @param buffer in the location specified in buffer->in_offs.
@@ -73,6 +92,11 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     // we don't care if buffer was empty or not during insertion
     buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
     buffer->entry[buffer->in_offs].size = add_entry->size;
+
+    //FOR TESTING
+    // printf("The added entry is:%s", buffer->entry[buffer->in_offs].buffptr);
+    // for (uint8_t i=0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i++) printf("%s", buffer->entry[i].buffptr);
+    // printf("\n\n");
 
     // increase in_offset by 1 after each insertion
     buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
